0e68be49a4fac1d1229b10b87430b106c8bd9c51
George Grainger <GraingerGY@cardiff.ac.uk>
2020-05-04 14:09:06 +0100

    Add RTCHandler class

---
+++ libraries/RTCHandler/RTCHandler.cpp
@@ -0,0 +1,132 @@
+#include <Arduino.h>
+#include "RTCHandler.h"
+
+//RTCHandler Constructor
+RTCHandler::RTCHandler()
+{
+    _rtc.begin();
+}
+
+/*
+    Sets time on the RTC to the given time (time must be 24 hour format).
+    Accounts for british daylight savings time.
+*/
+void RTCHandler::SetTime(DateTime time)
+{
+    _rtc.adjust(time);
+}
+
+/*
+This function formats the time according to daylight savings and 12 hour mode
+*/
+DateTime RTCHandler::FormatTime(DateTime time, bool twelveHour)
+{
+    //Get all the variables to handle wrap around for
+    int hour = time.hour();
+    int day = time.day();
+    int month = time.month();
+    int year = time.year();
+
+    //Transform them according to the current month (daylight savings in Britain)
+    if (isDaylightSavings(time))
+    {
+        //Add 1 to the hour
+        hour += 1;
+
+        //If the hour is greater than 24, wrap it around
+        // And add 1 to the day
+        if (hour > 24)
+        {
+            hour = hour % 24;
+            day++;
+
+            //If we have increase the hour, we must now check the day is correct
+            //If the day is greater than the length of the current month, wrap it around
+            //And increase the month by 1
+            if (day > dayinMonth[month])
+            {
+                //We modulus by the number of days in the month -1 so that it starts at 1 instead of 0.
+                day = day % (dayinMonth[month] - 1);
+                month++;
+
+                //If we have increased the month, we must now check the year
+                //If the month is greater than 12, then wrap it around
+                //And increase the year by 1
+                if (month > 12)
+                {
+                    month = month % 11;
+                    year++;
+                }
+            }
+        }
+    }
+
+    //Transform them according to the twelveHour boolean
+    if (twelveHour)
+    {
+        //Modulo the hour by 12, this will set it to the right value
+        hour = hour % 12;
+    }
+
+    //Return our corrected DateTime object
+    return DateTime(year, month, day, hour, time.minute(), time.second());
+}
+
+/*
+Function to get current time, accounts for daylight savings time and 12 hour mode
+*/
+DateTime RTCHandler::getTime(bool twelveHour)
+{
+    //Get the current date and time in a DateTime object
+    DateTime now = _rtc.now();
+
+    //Format the time
+    now = FormatTime(now, twelveHour);
+
+    return DateTime();
+}
+
+/*
+Function to get current time, accounts for daylight savings time
+*/
+DateTime RTCHandler::getTime()
+{
+    return getTime(false);
+}
+
+/*
+isAM returns true for AM times and false for PM times.
+*/
+bool RTCHandler::isAM()
+{
+    DateTime time = getTime();
+    if (time.hour() > 12)
+    {
+        return false;
+    }
+    return true;
+}
+
+/*
+Overload of isDaylightSavings(time).
+Returns true if the time is currently in daylight savings.
+Otherwise returns false.
+*/
+bool RTCHandler::isDaylightSavings()
+{
+    return isDaylightSavings(getTime());
+}
+
+/*
+Returns true if the time is currently in daylight savings.
+Otherwise returns false.
+param time - A datetime to check
+*/
+bool RTCHandler::isDaylightSavings(DateTime time)
+{
+    if (time.month() > 3 && time.month() < 10)
+    {
+        return true;
+    }
+    return false;
+}
---
+++ libraries/RTCHandler/RTCHandler.h
@@ -0,0 +1,38 @@
+#ifndef RTCHandler_h
+#define RTCHandler_h
+
+//Include the required librarys
+#include <Arduino.h>
+#include <RTClib.h>
+
+//Class for handling the RTC, automatically handles 12 hour time and daylight savings time.
+class RTCHandler
+{
+private:
+    RTC_DS3231 _rtc;
+
+public:
+    //Attributes
+    String dayofWeek[7] = {"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"};
+    int dayinMonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    //Constructors
+    RTCHandler();
+
+    //Functions
+    void SetTime(DateTime time);
+
+    //Get time functions
+    DateTime getTime();
+    DateTime getTime(bool twelveHour);
+
+    //Format time functions
+    DateTime FormatTime(DateTime time, bool twelveHour);
+
+    //Useful boolean functions
+    bool isAM();
+    bool isDaylightSavings(DateTime time);
+    bool isDaylightSavings();
+};
+
+#endif
