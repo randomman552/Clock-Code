e4cbb733998ba92b0d82cba4a290e5dc2b3fde54
George Grainger <GraingerGY@cardiff.ac.uk>
2020-05-15 14:40:41 +0100

    Switch to using TimeSpan objects

--- src/RTCHandler/RTCHandler.cpp
+++ src/RTCHandler/RTCHandler.cpp
@@ -14,60 +14,34 @@ RTCHandler::RTCHandler()
 
 void RTCHandler::SetTime(DateTime time)
 {
+    if (isDaylightSavings(time))
+    {
+        //Take away 1 hours to undo our corrections for daylight savings time.
+        time = time - TimeSpan(0, 1, 0, 0);
+    }
     _rtc.adjust(time);
 }
 
 DateTime RTCHandler::FormatTime(DateTime time, bool twelveHour)
 {
-    //Get all the variables to handle wrap around for
-    int hour = time.hour();
-    int day = time.day();
-    int month = time.month();
-    int year = time.year();
-
     //Transform them according to the current month (daylight savings in Britain)
     if (isDaylightSavings(time))
     {
-        //Add 1 to the hour
-        hour += 1;
+        //Add one hour
+        time = time + TimeSpan(0, 1, 0, 0);
+    }
 
-        //If the hour is greater than 24, wrap it around
-        // And add 1 to the day
-        if (hour > 24)
+    //If twelve hour is requested, take away 12 hours when the hour is greater than 12.
+    if (twelveHour)
+    {
+        if (time.hour() > 12)
         {
-            hour = hour % 24;
-            day++;
-
-            //If we have increase the hour, we must now check the day is correct
-            //If the day is greater than the length of the current month, wrap it around
-            //And increase the month by 1
-            if (day > dayinMonth[month])
-            {
-                //We modulus by the number of days in the month -1 so that it starts at 1 instead of 0.
-                day = day % (dayinMonth[month] - 1);
-                month++;
-
-                //If we have increased the month, we must now check the year
-                //If the month is greater than 12, then wrap it around
-                //And increase the year by 1
-                if (month > 12)
-                {
-                    month = month % 11;
-                    year++;
-                }
-            }
+            time = time - TimeSpan(0, 12, 0, 0);
         }
     }
 
-    //Transform them according to the twelveHour boolean
-    if (twelveHour)
-    {
-        //Modulo the hour by 12, this will set it to the right value
-        hour = hour % 12;
-    }
-
     //Return our corrected DateTime object
-    return DateTime(year, month, day, hour, time.minute(), time.second());
+    return time;
 }
 
 DateTime RTCHandler::getTime(bool twelveHour)
